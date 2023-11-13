fdbd2287f7a5412016d490ed46b956662effbac4
seengularSDUdent <200107026@stu.sdu.edu.kz>
2023-11-13 20:17:08 +0600

    Created an instance of Mug and Spring Context.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        Mug sampleMug = new Mug();
+    }
+}
---
+++ src/main/java/org/example/Mug.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Mug {
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
