6a55eee39a3e66904c4109ff8725f45f816be778
seengularSDUdent <200107026@stu.sdu.edu.kz>
2023-11-13 23:49:32 +0600

    Gave a identifiers(names/values) to beans and installed one of them as a primary making this bean as a default select with this type of bean

--- src/main/java/org/example/Main/Main.java
+++ src/main/java/org/example/Main/Main.java
@@ -7,6 +7,16 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
 
-        Mug sampleMug = context.getBean(Mug.class);
+        Mug primaryMug = context.getBean(Mug.class);
+        System.out.println(primaryMug.getName());
+
+        Mug sampleMug0 = context.getBean("ArmanMug", Mug.class);
+        System.out.println(sampleMug0.getName());
+
+        Mug sampleMug1 = context.getBean("DauletMug", Mug.class);
+        System.out.println(sampleMug1.getName());
+
+        Mug sampleMug2 = context.getBean("mug2", Mug.class);
+        System.out.println(sampleMug2.getName());
     }
 }
--- src/main/java/org/example/config/SpringConfiguration.java
+++ src/main/java/org/example/config/SpringConfiguration.java
@@ -3,18 +3,19 @@ package org.example.config;
 import org.example.Main.Mug;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class SpringConfiguration {
 
-    @Bean
+    @Bean(name = "ArmanMug")
     Mug mug0(){
         Mug beanMug = new Mug();
         beanMug.setName("Arman's Mug");
         return beanMug;
     }
 
-    @Bean
+    @Bean(value = "DauletMug")
     Mug mug1(){
         Mug beanMug = new Mug();
         beanMug.setName("Daulet's mug");
@@ -22,6 +23,7 @@ public class SpringConfiguration {
     }
 
     @Bean
+    @Primary
     Mug mug2(){
         Mug beanMug = new Mug();
         beanMug.setName("Gulnur's mug");
